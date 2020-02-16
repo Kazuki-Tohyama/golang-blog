308509823d0fc226969b6994387f717777fb8538
Kazuki-Tohyama <tonmas.thk@gmail.com>
2020-02-16 22:56:12 +0900

    add nuxt project

--- api/domain/article.go
+++ api/domain/article.go
@@ -1,9 +1,15 @@
 package domain
 
+import (
+	"time"
+)
+
 type Article struct {
-	ID int `gorm:"primary_key"`
-	Title string
-	Body string	
+	ID int `gorm:"column:id;primary_key"`
+	Title string `gorm:"column:title"`
+	Body string	`gorm:"column:body;type:text"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
 
 type Articles []Article 
--- mysql/initdb.d/create.sql
+++ mysql/initdb.d/create.sql
@@ -1,7 +1,7 @@
 create table IF not exists `articles`
 (
  `id`               INT AUTO_INCREMENT,
- `title`            VARCHAR(50) NOT NULL,
+ `title`            VARCHAR(100) NOT NULL,
  `body`             TEXT,
  `created_at`       Datetime DEFAULT NULL,
  `updated_at`       Datetime DEFAULT NULL,
